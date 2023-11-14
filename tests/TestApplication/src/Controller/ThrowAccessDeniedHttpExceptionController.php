de9f476a8adb871b1c56c08516c21c91792f34aa
Moshchenko Bohdan <bohdan.moshchenko@gmail.com>
2023-11-14 15:08:11 +0200

    Removed unused

--- tests/TestApplication/src/Controller/ThrowAccessDeniedHttpExceptionController.php
+++ tests/TestApplication/src/Controller/ThrowAccessDeniedHttpExceptionController.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace VisualCraft\RestBaseBundle\Tests\TestApplication\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 class ThrowAccessDeniedHttpExceptionController extends AbstractController
 {
-    public function __invoke(Request $request): void
+    public function __invoke(): void
     {
         $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
     }
