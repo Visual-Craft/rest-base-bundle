06d39251e73b7eb80ed1eb53513e26c9d60891e5
den <den@Denis.visual>
2025-04-03 15:50:46 +0300

    tests app config update

--- tests/TestApplication/config/services.php
+++ tests/TestApplication/config/services.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Psr\Log\NullLogger;
+use Symfony\Component\HttpKernel\Kernel;
 
 return static function (ContainerConfigurator $container): void {
     $services = $container->services()
@@ -18,11 +19,16 @@ return static function (ContainerConfigurator $container): void {
     ;
 
     $container->services()->set('logger', NullLogger::class);
+    $arguments = [
+        '$globalFactory' => service('limiter.ip_login'),
+        '$localFactory' => service('limiter.username_ip_login'),
+    ];
+
+    if (Kernel::MAJOR_VERSION >= 6) {
+        $arguments['$secret'] = 'secret';
+    }
+
     $container->services()->set('app.login_rate_limiter', 'Symfony\Component\Security\Http\RateLimiter\DefaultLoginRateLimiter')
-        ->args([
-            '$globalFactory' => service('limiter.ip_login'),
-            '$localFactory' => service('limiter.username_ip_login'),
-            '$secret' => 'secret',
-        ])
+        ->args($arguments)
     ;
 };
